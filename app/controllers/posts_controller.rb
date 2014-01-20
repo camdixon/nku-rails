6b317653511a653b261c8e71d4100b73dfb21c96
Mitch Lloyd <mitch.lloyd@gmail.com>
2014-01-19 22:28:36 -0500

    Validate post title presence and length

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,10 +7,18 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def new
+    @post = Post.new
+  end
+
   def create
     @post = Post.new(post_params)
-    @post.save
-    redirect_to @post
+
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
   end
 
   private

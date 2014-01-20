9c599f46c6046e9289bc4008ffa29132871229cc
Mitch Lloyd <mitch.lloyd@gmail.com>
2014-01-19 22:55:56 -0500

    Add authentication for unsafe actions

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,6 @@
 class CommentsController < ApplicationController
+  http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
+
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create(params[:comment])
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+
   def index
     @posts = Post.all
   end

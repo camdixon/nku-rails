7260a1a0f650612e98495fd617e27f895042c793
Mitch Lloyd <mitch.lloyd@gmail.com>
2014-01-23 09:01:12 -0500

    Rename phonetic name to nickname

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140122031137) do
+ActiveRecord::Schema.define(version: 20140123135814) do
 
   create_table "comments", force: true do |t|
     t.string   "commenter"
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(version: 20140122031137) do
 
   create_table "students", force: true do |t|
     t.string "name"
-    t.string "phonetic_name"
+    t.string "nickname"
     t.string "email"
     t.string "image_url"
   end
--- spec/support/pages/new_student_page.rb
+++ spec/support/pages/new_student_page.rb
@@ -3,7 +3,7 @@ class NewStudentPage
 
   def create_student(student)
     fill_in 'Name', with: student.name
-    fill_in 'Phonetic Name', with: student.phonetic_name
+    fill_in 'Nickname', with: student.nickname
     fill_in 'Email', with: student.email
     fill_in 'Image URL', with: student.image_url
     click_button 'Create this student'

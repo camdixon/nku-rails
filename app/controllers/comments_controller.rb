d6cf72b4a74fd57328910e96683ec722b48e8021
Kevin Rockwood <kevin@rockwood.me>
2014-01-30 09:22:21 -0500

    Fix permitted params on comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,7 @@ class CommentsController < ApplicationController
 
   def create
     @post = Post.find(params[:post_id])
-    @comment = @post.comments.create(params[:comment])
+    @comment = @post.comments.create(comment_params)
     redirect_to post_path(@post)
   end
 
@@ -12,5 +12,11 @@ class CommentsController < ApplicationController
     @comment = @post.comments.find(params[:id])
     @comment.destroy
     redirect_to post_path(@post)
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:commenter, :body)
   end
 end

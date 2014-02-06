e975a3e72eb8a3d878b76a4d54930887fcb92f07
Mitch Lloyd <mitch.lloyd@gmail.com>
2014-02-05 21:18:39 -0500

    Add numericality seat validations

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -2,4 +2,9 @@ class Attendance < ActiveRecord::Base
   belongs_to :student
 
   validates :attended_on, uniqueness: {scope: :student_id, message: "you already logged your attendance today"}
+  validates :seat, numericality: {
+    only_integer: true,
+    greater_than_or_equal_to: 1,
+    less_than_or_equal_to: 4
+  }
 end

2671711135367616eb60565363901ff2d8fc35d1
Mitch Lloyd <mitch.lloyd@gmail.com>
2014-02-12 12:46:56 -0500

    Stop setting the user's password in feature specs
    It seems backwards to set the user's password in order to use the wrong
    password.

--- spec/features/signing_in_spec.rb
+++ spec/features/signing_in_spec.rb
@@ -15,9 +15,8 @@ feature 'Signing In' do
 
   context "invalid password" do
     scenario do
-      student.password = "not-my-password"
       new_session_page.visit_page
-      new_session_page.sign_in(student)
+      new_session_page.sign_in(student, 'not-my-password')
       expect(new_session_page).to_not be_successful
       expect(navbar_section).to_not be_signed_in
     end
--- spec/support/pages/new_session_page.rb
+++ spec/support/pages/new_session_page.rb
@@ -6,9 +6,9 @@ class NewSessionPage
     self
   end
 
-  def sign_in(student)
+  def sign_in(student, password="password")
     fill_in 'Email', with: student.email
-    fill_in 'Password', with: student.password || "password"
+    fill_in 'Password', with: password
     click_button 'Sign in'
   end
 

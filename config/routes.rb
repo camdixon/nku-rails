0372b6da79c94c5ce9ab40a1769595e3503b0f80
Mitch Lloyd <mitch.lloyd@gmail.com>
2014-02-25 20:13:28 -0500

    Remove blog features

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 NkuRails::Application.routes.draw do
-  resources :posts do
-    resources :comments
-  end
-
   resources :students, except: :edit do
     resources :attendances
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,29 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140204145841) do
+ActiveRecord::Schema.define(version: 20140226010851) do
 
   create_table "attendances", force: true do |t|
     t.integer  "seat"
     t.date     "attended_on"
     t.integer  "student_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "comments", force: true do |t|
-    t.string   "commenter"
-    t.text     "body"
-    t.integer  "post_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "comments", ["post_id"], name: "index_comments_on_post_id"
-
-  create_table "posts", force: true do |t|
-    t.string   "title"
-    t.text     "text"
     t.datetime "created_at"
     t.datetime "updated_at"
   end

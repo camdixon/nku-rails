29a8b5eb02d8aec1d877c3b692d392f07d5d7653
Cameron Dixon <cam.dixon@gmail.com>
2014-03-29 20:32:41 +0000

    update on finding a student by email in assignments controller

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -23,6 +23,8 @@ class AssignmentsController < ApplicationController
   end
   
   def upload
+    found_existing = false
+    @assignments = Assignment.all
     csv_text = params[:upload].read
     csv = CSV.parse(csv_text, :headers => true)
     count = 0
@@ -30,11 +32,23 @@ class AssignmentsController < ApplicationController
       #find a student by email and tie to student
       #skip line if it has any blank values
       next if row["email"] == nil || row["score"] == nil || row["name"] == nil || row["total"] == nil
+      student_email = Student.find_by_email(row["email"])
+      #if we do not have the above student in our database, skip over him
+      next if student_email == nil
+      
+      debugger
       #update score if student's email and assignment name already exist
-      #here
+      @assignments.each do |assignment|
+        if assignment.name == row["name"] && student_email.email == row["email"]
+          found_existing = true
+          assignment.score =  row["score"]
+          assignment.total =  row["total"]          
+        end
+      end
+      next if found_existing == true
+      
       #otherwise, add in the new assignment below
-      student = Student.find_by_email(params[row["email"]])
-      @assignment = Assignment.create!(name:row["name"], total:row["total"], score:row["score"])
+      @assignment = Assignment.create!(name:row["name"], student_id:student_email.id, total:row["total"], score:row["score"])
       count += 1
     end
     redirect_to assignments_path, notice: "#{count} assignments were created"
